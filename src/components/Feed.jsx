46be38d68d972b92729e8551d1b88d5fc6c0d11f
Karina Cristo <karinacristonegocios@gmail.com>
2022-09-24 12:49:18 -0300

    chamando a api de videos e configurando

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
+
+    const [selectedCategory,
+        setSelectedCategory] = useState('New');
+
+    useEffect(() => {
+        fetchFromAPI('search?part=snippet&q=${selectedCategory}');
+    }, [selectedCategory]);
+
     return (
         <Stack sx={{
             flexDirection: {
@@ -21,7 +30,12 @@ const Feed = () => {
                     md: 2
                 }
             }}>
-                <Sidebar />
+                <Sidebar
+                    selectedCategory=
+                    {selectedCategory}
+                    setSelectedCategory=
+                    {setSelectedCategory}
+                />
                 <Typography className='copyright'
                     variant='body2' sx={{
                         mt: 1.5,
@@ -29,6 +43,18 @@ const Feed = () => {
                     }} >
                     Copyright 2022 Karina Cristo
                 </Typography>
+            </Box>
+            <Box p={2} sx={{
+                overflowY: 'auto',
+                height: '90vh', flex: 2
+            }}>
+                <Typography variant='h4' fontWeight='bold' mb={2}
+                    sx={{ color: 'white' }}>
+                    {selectedCategory} <span style={{ color: '#F31503' }}>
+                        videos
+                    </span>
+                </Typography>
+                <Videos videos={[]} />
             </Box>
         </Stack>
     )
