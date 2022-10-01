ab3e638af54ff07157fb4e297f0312da4e4a3878
Karina Cristo <karinacristonegocios@gmail.com>
2022-09-30 21:44:21 -0300

    configurações de layout dos videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
-import { ChannelCard, Loader, VideoCard } from "./";
-
-const Videos = ({ videos, direction }) => {
-    if (!videos?.length) return <Loader />;
+const Videos = ({ videos }) => {
 
     return (
-        <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+        <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
             {videos.map((item, idx) => (
                 <Box key={idx}>
                     {item.id.videoId && <VideoCard video={item} />}
@@ -15,6 +13,7 @@ const Videos = ({ videos, direction }) => {
                 </Box>
             ))}
         </Stack>
+
     );
 }
 
